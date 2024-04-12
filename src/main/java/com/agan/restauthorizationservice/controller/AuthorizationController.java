96c5ec7b8a6e305b5d3dd2192450242a2a363fc2
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-12 22:28:29 +0300

    authorizationController added

--- src/main/java/com/agan/restauthorizationservice/controller/AuthorizationController.java
+++ src/main/java/com/agan/restauthorizationservice/controller/AuthorizationController.java
@@ -1,11 +1,27 @@
 package com.agan.restauthorizationservice.controller;
 
+import com.agan.restauthorizationservice.enums.Authorities;
+import com.agan.restauthorizationservice.service.AuthorizationService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class AuthorizationController {
 
+    private final AuthorizationService authorizationService;
 
+    @GetMapping("/authorize")
+    public ResponseEntity<List<Authorities>> getAuthorities(
+            @Validated @RequestParam("user") String user, @Validated @RequestParam("password") String password) {
+
+        List<Authorities> authorities = authorizationService.getAuthorities(user, password);
+        return ResponseEntity.ok(authorities);
+    }
 }
