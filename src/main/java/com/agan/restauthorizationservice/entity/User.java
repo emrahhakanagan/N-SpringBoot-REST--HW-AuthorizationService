50782d2f73269a9aaf18ab135339f9cb7f2cec5f
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-12 23:29:44 +0300

    User class uptaded

--- src/main/java/com/agan/restauthorizationservice/entity/User.java
+++ src/main/java/com/agan/restauthorizationservice/entity/User.java
@@ -1,9 +1,20 @@
 package com.agan.restauthorizationservice.entity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class User {
+    @NotBlank(message = "Username cannot be empty")
+    @Length(min = 6, message = "Username must be at least 6 characters long")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Username must not include special characters")
     private String username;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Length(min = 6, message = "Password must be at least 6 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}$",
+            message = "Password must include digits, lower and upper case letters")
     private long password;
 }
