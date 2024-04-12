9a1c018446cfe10d59fb67601368214a6a5e071f
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-12 22:26:46 +0300

    exceptionHandlerAdvice added

--- src/main/java/com/agan/restauthorizationservice/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/agan/restauthorizationservice/exception/ExceptionHandlerAdvice.java
@@ -1,8 +1,38 @@
 package com.agan.restauthorizationservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<?> handleInvalidCredentials(InvalidCredentials invalidCredentials) {
+        List<String> details = new ArrayList<>();
+        details.add(invalidCredentials.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse("Invalid Credentials", details);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<?> handleUnauthorizedUser(UnauthorizedUser unauthorizedUser) {
+        List<String> details = new ArrayList<>();
+        details.add(unauthorizedUser.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse("Unauthorized User", details);
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(errorResponse);
+    }
 
 }
