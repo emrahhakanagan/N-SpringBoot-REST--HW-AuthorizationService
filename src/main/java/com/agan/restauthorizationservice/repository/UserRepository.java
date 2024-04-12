4b523d1b79e4cc0b4a1c11e627b911d4e68acc47
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-12 22:17:45 +0300

    userRepository added

--- src/main/java/com/agan/restauthorizationservice/repository/UserRepository.java
+++ src/main/java/com/agan/restauthorizationservice/repository/UserRepository.java
@@ -1,8 +1,27 @@
 package com.agan.restauthorizationservice.repository;
 
+import com.agan.restauthorizationservice.enums.Authorities;
 import org.springframework.stereotype.Repository;
+
+import java.util.*;
 
 @Repository
 public class UserRepository {
 
+    private Map<String, String> allUsers;
+
+    public UserRepository() {
+        allUsers = new HashMap<>();
+        allUsers.put("aganagan", "Agan123");
+        allUsers.put("hakanagan", "HakanAgan123");
+        allUsers.put("emrahhakan", "emrahhakan123");
+    }
+
+    public List<Authorities> getUserAuthorities(String user, String password) {
+        if (allUsers.containsKey(user) && allUsers.get(user).equals(password)) {
+            return Arrays.asList(Authorities.READ, Authorities.WRITE, Authorities.DELETE);
+        } else {
+            return Collections.emptyList();
+        }
+    }
 }
